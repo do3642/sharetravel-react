cb1e5ccd1c757c64dc19b1050ea61cfe0c5505a1
yeong <chlgkdud111@gmail.com>
2024-11-13 16:38:56 +0900

    Header.js로 기본적인 헤더에 들어가야 되는 요소들 작업

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+function Header() {
+  return(
+    <>
+      <nav>
+        <h1>로고 여정</h1>
+        <ul>
+          <li>국내</li>
+          <li>국외</li>
+          <li>추천</li>
+          <li>나만의 팁</li>
+          <li>중고거래</li>
+        </ul>
+        <input type="search"></input>
+        <button>검색</button>
+        <ul>
+          <li>로그인</li>
+          <li>회원가입</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Header;
