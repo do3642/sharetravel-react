b6bfa892ba90a7867d1ed5ce712fd1ceb24a688d
yc <do36421@naver.com>
2024-11-14 11:36:06 +0900

    add: visual컴포,기본css,폴더 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import { Routes } from 'react-router-dom';
 import './App.css';
+import Visual from './components/Visual';
 
 function App() {
   return (
     <div className="App">
-     
+
+      <Visual />
      <Routes>
 
 
