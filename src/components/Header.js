31045880035dd6470d90a0a2f4929199dcb7ea7e
yeong <chlgkdud111@gmail.com>
2024-11-14 10:40:35 +0900

    Header.js 수정, Header.css로 요소들의 대략적인 위치 잡음.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,22 @@ import './Header.css'
 function Header() {
   return(
     <nav>
-      <a href='#' class="nav-loggo"><h1>로고 여정</h1></a>
-      <ul class="nav-menu">
+      <a href='#' className="nav-loggo">
+        <h1>로고</h1> 
+        <h1>여정</h1>
+      </a>
+      <ul className="nav-menu">
         <li><a href='#'>여행정보</a></li>
         <li><a href='#'>여행팁</a></li>
         <li><a href='#'>여행지 추천</a></li>
         <li><a href='#'>마켓</a></li>
       </ul>
-      <div class="nav-right">
-        <div class="nav-search">
+      <div className="nav-right">
+        <div className="nav-search">
           <input type="search"></input>
           <button>검색</button>
         </div>
-        <ul class="nav-user">
+        <ul className="nav-user">
           <li><a href='#'>로그인</a></li>
           <li><a href='#'>회원가입</a></li>
         </ul>
