5657c614b9788a9eda4f492711c4e487dc089419
yeong <chlgkdud111@gmail.com>
2024-11-14 18:35:47 +0900

    Header에 있는 검색창 창이 줄어들어도 망가지지 않게 처리해줌.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Visual from './components/Visual';
+import { useState } from 'react';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false); // false면 로그아웃 상태, true면 로그인 상태.
   return (
     <div className="App">
-      <Header />
+      <Header isAuth={isAuth}/>
 
       <Visual />
      <Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,25 @@
 import '../styles/Header.css'
 import AccountMenu from './AccountMenu';
 
-function Header() {
+function Header({ isAuth }) {
   return(
     <nav>
-      <a href='#' className="nav-title">
-        <h1>여정</h1>
-      </a>
-      <ul className="nav-menu">
-        <li><a href='#'>여행정보</a></li>
-        <li><a href='#'>여행팁</a></li>
-        <li><a href='#'>여행지 추천</a></li>
-        <li><a href='#'>마켓</a></li>
-      </ul>
+        <a href='#' className="nav-title">
+          <h1>"여정"</h1>
+        </a>
+        <ul className="nav-menu">
+          <li><a href='#'>여행정보</a></li>
+          <li><a href='#'>여행팁</a></li>
+          <li><a href='#'>여행지 추천</a></li>
+          <li><a href='#'>마켓</a></li>
+        </ul>
       <div className="nav-right">
         <div className="nav-search">
           <input type="search"></input>
           <button>검색</button>
         </div>
         <ul className="nav-user">
-          <AccountMenu />
+          <AccountMenu isAuth={isAuth}/>
         </ul>
       </div>
     </nav>
