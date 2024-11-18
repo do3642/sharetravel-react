523972d41e74384cc93ce04712c586c58f36f744
yc <do36421@naver.com>
2024-11-18 12:05:01 +0900

    Style: Travel-info 반응형수정

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import TravelAds from './components/TravelAds';
-// import Visual from './components/Visual';
+import Visual from './components/Visual';
 import { useState } from 'react';
-// import TravelInfo from './components/TravelInfo';
+import TravelInfo from './components/TravelInfo';
 
 function App() {
   const [isAuth, setIsAuth] = useState(false); // false면 로그아웃 상태, true면 로그인 상태.
@@ -18,8 +18,8 @@ function App() {
 
 
      </Routes>
-     {/* <Visual /> */}
-     {/* <TravelInfo/> */}
+     <Visual />
+     <TravelInfo/>
      <TravelAds/>
 
 
