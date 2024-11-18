d682bc3a373d1114dd4c79edefe9119f15e4d30b
yc <do36421@naver.com>
2024-11-18 11:22:19 +0900

    Add: 더미데이터로 8개 반복 처리 Style: flex로 반응형 처리

--- src/components/TravelAds.js
+++ src/components/TravelAds.js
@@ -1,9 +1,38 @@
 import TravelAdsBox from "./TavelAdsBox";
+import adsData from "../data/travelAdsData.js";
+import { Link } from "react-router-dom";
+import '../styles/TravelAds.css'
+
 
 function TravelAds(){
+  // 4개씩 그룹화
+  const groupedData = adsData.reduce((acc, curr, index) => {
+    if (index % 4 === 0) acc.push([]); // 새로운 그룹 시작
+    acc[acc.length - 1].push(curr); // 현재 데이터를 마지막 그룹에 추가
+    return acc;
+  }, []);
+
+
   return(
     <section className="Travel-Ads">
-      <TravelAdsBox />
+      <div className="Travel-Ads-Top">
+        <h2>제목</h2>
+        <Link to={"/"}>+더보기</Link>
+      </div>
+      <article>
+         {adsData.map((ad) => (
+        <TravelAdsBox
+          key={ad.id}
+          imgSrc={ad.imgSrc}
+          category={ad.category}
+          title={ad.title}
+          description={ad.description}
+          detail={ad.detail}
+          likes={ad.likes}
+          comments={ad.comments}
+         />
+            ))}
+        </article>
     </section>
   )
 }
